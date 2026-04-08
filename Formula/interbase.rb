a77925a8dac803f2a3287a2e14d0dc3dbee5eb73
Rehat Kathuria <hello@reh.at>
2026-04-07 23:17:29 -0400

    Release interbase 0.0.4

--- Formula/interbase.rb
+++ Formula/interbase.rb
@@ -1,9 +1,9 @@
 class Interbase < Formula
   desc "Interbase command-line interface"
   homepage "https://github.com/agentsorchestrationcompany/cli"
-  url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.3/interbase-cli-v0.0.3-homebrew.tar.gz"
-  sha256 "ec1006912d3b458940a4fa258749d5fedeb2c4a185d3e0f17819842ada0742c9"
-  version "0.0.3"
+  url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.4/interbase-cli-v0.0.4-homebrew.tar.gz"
+  sha256 "751283bdbe18c4c3047c1d6f3a59afd27f67269d5ce8bac826ad6bcf09db3dc0"
+  version "0.0.4"
   depends_on "node"
 
   def install
