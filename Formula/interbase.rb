cd0bf19e50b502a59cdba49438fdd87aea4e16a5
Rehat Kathuria <hello@reh.at>
2026-05-18 22:53:24 -0400

    Update interbase to v0.10.0

--- Formula/interbase.rb
+++ Formula/interbase.rb
@@ -4,30 +4,30 @@
 class Interbase < Formula
   desc "Interbase CLI"
   homepage "https://github.com/agentsorchestrationcompany/cli"
-  version "0.0.1"
+  version "0.10.0"
   depends_on "ripgrep"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.1/interbase-darwin-x64.zip"
-      sha256 "9eb9d7a13da98ccfcb4ba9fc85b1e6964de7c931990482a091fc522e6a2c5ae8"
+      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.10.0/interbase-darwin-x64.zip"
+      sha256 "c739fe5eaef9a5de87594e9b58b0ced4d3ad76f2edd8d386954d7c90724462fe"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.1/interbase-darwin-arm64.zip"
-      sha256 "79d9e14f7e1d931dfd461f720d205609927074b1d08025243f4ac6df6a03ea16"
+      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.10.0/interbase-darwin-arm64.zip"
+      sha256 "b8e9f9e62e559e14087c7e7fc06c5eefb629478693f46c344d862cef1127fa4e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.1/interbase-linux-x64.tar.gz"
-      sha256 "b14b3798c1cb2dd1db1098a1d7ad9f8fcef47f132e4f0dc80b2f2fffbb6eb7e2"
+      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.10.0/interbase-linux-x64.tar.gz"
+      sha256 "44d66ead3c43be41a0a8cffd6c8ef7a722375507d3183834f905df40126426f9"
     end
 
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.0.1/interbase-linux-arm64.tar.gz"
-      sha256 "2a1b8c2164add0a89ce8d2e4e2a765a7a4cc4ead04ce22f701277fd7018140ab"
+      url "https://github.com/agentsorchestrationcompany/cli/releases/download/v0.10.0/interbase-linux-arm64.tar.gz"
+      sha256 "ba45b6ba37529125791fabadd8c4580ab8ef1b7a260cb5d29e7a4bd532e51a6a"
     end
   end
 
